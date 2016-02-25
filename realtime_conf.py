Fixed LWA Formula
Fixed LWA formula - it now uses the M-Climatology and not the resulting standardized anomalies for gradient and mean. Also cleaned up the code a bit and patched a slight bug with M-Climate .npy files. Will need to fix M-Climates another time. Removed some test functions and started the process of getting rid of other bloat in the main M-Climate generating/LWA function.

realtime_conf.py
@@ -13,6 +13,7 @@ import scipy.ndimage as ndimage
 from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 import random
 
+##Initiate variables used in multiple functions
 scriptstart = dt.datetime.utcnow()
 now = datetime.utcnow()
 nowetc = datetime.now()
@@ -20,6 +21,8 @@ lats = np.linspace(23,52,30)
 lons = np.linspace(233,295,63)
 anomcolors=[]
 
+
+##Color map provided by Trevor Alcott
 anomcolors.append([240,140,250])
 anomcolors.append([200,40,160])
 anomcolors.append([120,0,200])
@@ -40,31 +43,14 @@ anomcolors.append([255,0,0])
 anomcolors.append([255,170,170])
 anomcolors2=anomcolors[9:18]
 
-'''anomcolors.append([255,170,170])
-anomcolors.append([255,0,0])
-anomcolors.append([170,0,0])
-anomcolors.append([100,0,0])
-anomcolors.append([180,140,30])
-anomcolors.append([240,200,40])
-anomcolors.append([255,255,20])
-anomcolors.append([245,255,160])
-anomcolors.append([255,255,255])
-anomcolors.append([255,255,255])
-anomcolors.append([180,255,180])
-anomcolors.append([80,255,80])
-anomcolors.append([30,160,30])
-anomcolors.append([0,90,0])
-anomcolors.append([0,50,255])
-anomcolors.append([100,100,255])
-anomcolors.append([180,180,255])
-anomcolors.append([235,235,255])
-anomcolors2=anomcolors[9:18]'''
-
 anomcolors=(np.array(anomcolors)/255.0).tolist()
 anomcolors2=(np.array(anomcolors2)/255.0).tolist()
 anomcmap=ListedColormap(anomcolors,name='anommap',N=None)
 anomcmap2=ListedColormap(anomcolors2,name='anommap2',N=None)
 
+
+##Gradient fuction finds magnitude of climatology gridpoint at point (y,z), i.e. subtracts and squares (x+1, x-1), (y+1, y-1), adds those and sqrt the resulting value.
+##Corners of the plot are created with (x+1,x), (y+1,y), (x,x-1), (y,y-1)
 def gradFunc(x,y,z,mean630):
     a=mean630[x,y,z]
     if y == 0 and z == 0:
@@ -82,7 +68,8 @@ def gradFunc(x,y,z,mean630):
     else:
         grad=math.sqrt((mean630[x,y+1,z]-mean630[x,y-1,z])**2+(mean630[x,y,z+1]-mean630[x,y,z-1])**2)
     return grad
-
+    
+##Realtime GEFS gradient function
 def gfrt(y,z,fm):
     a=fm[y,z]
     if y == 0 and z == 0:
@@ -118,23 +105,23 @@ def boxfuncg(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630
     
     return gboxbin[(631+x*4),y,z]
     
-def boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630):
+def boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630, c):
     if y+q < 0:
         return np.nan
     if z+w < 0:
         return np.nan
     if y+q >=30 or z+w >=63:
         return np.nan
-       
+    u = 631+x*4   
     gradMCli = ((gm[x,y+q,z+w] - meanG)/stdG)
     if ((gradzscore[y,z]-1) <= gradMCli <= (gradzscore[y,z]+1)):
         
-        boxbin[(631+x*4),y,z]=spread630[x,y+q,z+w]
+        gboxbin[u,y,z]=spread630[x,y+q,z+w]
         
     
 
 
-    return boxbin[(631+x*4),y,z]
+    return gboxbin[u,y,z]
 def boxfunc(x,y,z,q,mean630,meanM,stdM,zscore,boxbin,spread630,w):
     
     if y+q < 0:
@@ -886,6 +873,11 @@ def confPm4m(mslpMean, mslpStd, time, run, day):
         print 'finished plot number ' +str(t+1)+'!'                                    
         plt.close('all')      
           
+          
+
+
+
+
 def confPm4lw(mslpMean, mslpStd, time, run, day):
     forecastSprdGrid = mslpStd
     forecastMeanGrid = mslpMean
@@ -899,8 +891,15 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
         if hour > 168:
             break
         mean630 = np.load('/media/taylor/Storage/mcli/'+str(month)+'_'+str(day)+'_/'+str(month)+'_'+str(day)+'_'+str(hour)+'z_mslp_mean.npy')
-
+      
         spread630 = np.load('/media/taylor/Storage/mcli/'+str(month)+'_'+str(day)+'_/'+str(month)+'_'+str(day)+'_'+str(hour)+'z_mslp_sprd.npy')
+        for x in range (0,630):
+            for y in range (0,30):
+                for z in range (0,63):
+                    if mean630[x,y,z] < 89000:
+                        mean630[x,y,z] = np.nan
+                        spread630[x,y,z] = np.nan
+        
         boxbin = np.empty((630*6,30,63))
         gm = np.empty((630,30,63))
         gm2 = np.empty((630,30,63))
@@ -928,15 +927,15 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
                 for x in range(0,630):
                     gm[x,y,z] = gradFunc(x,y,z,mean630)
                 
-                stdG = np.std(gm[:,y,z])
-                meanG = np.mean(gm[:,y,z])
+                stdG = np.nanstd(gm[:,y,z])
+                meanG = np.nanmean(gm[:,y,z])
                 
                 rtgm[y,z] = gfrt(y,z,forecastMeanGrid[t,:,:])
                 
-                stdM = np.std(binnedmean)
-                meanM = np.mean(binnedmean)
-                stdS = np.std(binnedsprd)
-                meanS = np.mean(binnedsprd)
+                stdM = np.nanstd(binnedmean)
+                meanM = np.nanmean(binnedmean)
+                stdS = np.nanstd(binnedsprd)
+                meanS = np.nanmean(binnedsprd)
                 
                 gradzscore[y,z] = (rtgm[y,z] -meanG)/stdG
                 zscore[y,z]= (forecastMeanGrid[t,y,z] - meanM)/stdM
@@ -946,21 +945,57 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
                     
                     mCliZScore = ((mean630[x,y,z] - meanM)/stdM)
                     gradMCli = ((gm[x,y,z] - meanG)/stdG)
-                    
-                                
-                    if ((gradzscore[y,z]-1) <= gradMCli <= (gradzscore[y,z]+1)): #if the point of interest (case, x, y) has a zscore relative to all cases at that point (630, x, y) within 1 of the point on the forecast grid (forecast, x, y), add to bin
-                        gboxbin[x,y,z] = spread630[x,y,z]
-                        if y in range (15,20):
-                            if z in range (44,49):
-                                gtestarray[t,x,:,:] = mean630[x,:,:]
-                    else:
-                        for q in range (-2,3):
-                            for w in range (-2,3):
+                    c=0
+                    if gradMCli < 3:            
+                        if ((gradzscore[y,z]-1) <= gradMCli <= (gradzscore[y,z]+1)): #if the point of interest (case, x, y) has a zscore relative to all cases at that point (630, x, y) within 1 of the point on the forecast grid (forecast, x, y), add to bin
+                            gboxbin[x,y,z] = spread630[x,y,z]
+                            c+=1
+
+                        else:
+                            for q in range (-2,3):
+                                for w in range (-2,3):
+
+                                    gboxbin[(631+x*4),y,z] = boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630,c)
+                                    if gboxbin[(631+x*4),y,z] > -5000:
+                                        c+=1
+                    elif gradMCli > 3:
+                        if ((gradzscore[y,z]-1) <= gradMCli <= (gradzscore[y,z]+2)): #if the point of interest (case, x, y) has a zscore relative to all cases at that point (630, x, y) within 1 of the point on the forecast grid (forecast, x, y), add to bin
+                            gboxbin[x,y,z] = spread630[x,y,z]
+                            c+=1
+
+                        else:
+                            for q in range (-2,3):
+                                for w in range (-2,3):
+                                    
+                                    gboxbin[(631+x*4),y,z] = boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630,c)
+                                    if gboxbin[(631+x*4),y,z] > -5000:
+                                        c+=1
+                    elif gradMCli > 5:
+                        if ((gradzscore[y,z]-1) <= gradMCli <= (gradzscore[y,z]+50)): #if the point of interest (case, x, y) has a zscore relative to all cases at that point (630, x, y) for all cases with large anomaly (forecast, x, y), add to bin
+                            gboxbin[x,y,z] = spread630[x,y,z]
+                            c+=1
+                        else:
+                            for q in range (-2,3):
+                                for w in range (-2,3):
+                                    
+                                    gboxbin[(631+x*4),y,z] = boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630,c)
+                                    if gboxbin[(631+x*4),y,z] > -5000:
+                                        c+=1
+                if c <= 5:
+                    for x in range (0,630):
+                        mCliZScore = ((mean630[x,y,z] - meanM)/stdM)
+                        gradMCli = ((gm[x,y,z] - meanG)/stdG)
+                        c=0
+                        for q in range (-4,5):
+                            for w in range (-4,5):
                                 if y in range (15,20):
                                     if z in range (44,49):
                                         gtestarray[t,x,:,:] = mean630[x,:,:]
-                                gboxbin[(631+x*4),y,z] = boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630)         
-        gradmean = np.mean(gm,axis=0)       
+                                gboxbin[(631+x*4),y,z] = boxfuncgs(x,y,z,q,mean630,meanG,stdG,gradzscore,gboxbin,gm,w,boxbin,spread630,c)
+                                if gboxbin[(631+x*4),y,z] > -5000:
+                                    c+=1
+                                                        
+        gradmean = np.nanmean(gm,axis=0)       
         mCliMean = np.nanmean(boxbin, axis=0)
         mCliStd =  np.nanstd(boxbin,axis=0)
         
@@ -980,15 +1015,15 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
                 for x in range(0,630):
                     gm[x,y,z] = gradFunc(x,y,z,mean630)
                 
-                stdG = np.std(gm[:,y,z])
-                meanG = np.mean(gm[:,y,z])
+                stdG = np.nanstd(gm[:,y,z])
+                meanG = np.nanmean(gm[:,y,z])
                 
                 rtgm[y,z] = gfrt(y,z,forecastMeanGrid[t,:,:])
                 
-                stdM = np.std(binnedmean)
-                meanM = np.mean(binnedmean)
-                stdS = np.std(binnedsprd)
-                meanS = np.mean(binnedsprd)
+                stdM = np.nanstd(binnedmean)
+                meanM = np.nanmean(binnedmean)
+                stdS = np.nanstd(binnedsprd)
+                meanS = np.nanmean(binnedsprd)
                 
                 gradzscore[y,z] = (rtgm[y,z] -meanG)/stdG
                 zscore[y,z]= (forecastMeanGrid[t,y,z] - meanM)/stdM
@@ -1000,46 +1035,19 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
                     
                                 
                     if ((zscore[y,z]-1) <= mCliZScore <= (zscore[y,z]+1)): #if the point of interest (case, x, y) has a zscore relative to all cases at that point (630, x, y) within 1 of the point on the forecast grid (forecast, x, y), add to bin
-                        if y in range (15,20):
-                            if z in range (44,49):
-                                testarray[t,x,:,:] = mean630[x,:,:]
                         boxbin[x,y,z] = spread630[x,y,z]
-                        if mCliZScore > 3:
-                            print x, y, z
+        
                     else:
                         for q in range (-2,3):
-                            for w in range (-2,3):
-                                if mCliZScore > 3:
-                                    print x, y, z
-                                if y in range (15,20):
-                                    if z in range (44,49):
-                                        testarray[t,x,:,:] = mean630[x,:,:]
+                            for w in range (-2,3):                             
                                 boxbin[(631+x*4),y,z] = boxfunc(x,y,z,q,mean630,meanM,stdM,zscore,boxbin,spread630,w)
                                 
-        gradmean = np.mean(gm,axis=0)       
+        gradmean = np.nanmean(gm,axis=0)       
         mCliMean = np.nanmean(boxbin, axis=0)
         mCliStd =  np.nanstd(boxbin,axis=0)
         gradMCliMean = np.nanmean(gboxbin,axis=0)
         gradMCliStd = np.nanstd(gboxbin,axis=0)
-        for x in range (0,3780):
-            if np.nanmean(boxbin[x,:,:]) and np.nanmean(gboxbin[x,:,:]) > 0:
-                 if np.nanmean(boxbin[x,:,:]) == np.nanmean(gboxbin[x,:,:]):
-                    print 'match at ' + str(x)
-                    m1 = Basemap(llcrnrlon=265,llcrnrlat=lats.min(),urcrnrlon=lons.max(),urcrnrlat=lats.max(),projection='cyl',resolution='i',ax=ax2)
-            
-                    x,y = m1(*np.meshgrid(lons,lats))
-                    mslspace = range(900,1100,4)
-                    z0 = m1.contourf(x,y,boxbin[x,:,:]/100,cmap=anomcmap)
-                    cbar = m1.colorbar(z0,ticks=mslspace,location='bottom',pad='5%')
-                    z0.cmap.set_over('#551A8B') 
-                    cbar.set_label(r'$\frac{F_g-\mu}{\sigma}$')
-                    z1 = m1.contour(x,y,boxbin[x,:,:]/100,levels=mslspace,colors='k',linewidths=1)
-                    plt.clabel(z1, fontsize=10, inline_spacing=-0.5,fmt='%3.0f')
-                    m1.drawcountries()
-                    m1.drawcoastlines()
-                    m1.drawstates()
-                    plt.savefig('/media/taylor/Storage/mcli/rftest/mslp_gradandmean'+str(x)+'.png')
-                    plt.close('all')
+ 
         for lat in range (0,30):
             for lon in range (0,63):
                 if mCliStd[lat,lon] > 1000:
@@ -1053,25 +1061,19 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
             
         forecastSprdGrid = np.array(forecastSprdGrid)            
         gradanomaly = (forecastSprdGrid[t,:,:]-gradMCliMean[:,:])/gradMCliStd[:,:]                  
-        meananomaly = (forecastMeanGrid[t,:,:] - np.mean(mean630,axis=0))/np.std(mean630,axis=0)
-        gradientAnomaly = (rtgm-np.mean(gm,axis=0))/np.std(gm,axis=0)   
-        spreadz = (forecastSprdGrid[t,:,:] - np.mean(spread630,axis=0))/np.std(spread630,axis=0)         
+        meananomaly = (forecastMeanGrid[t,:,:] - np.nanmean(mean630,axis=0))/np.nanstd(mean630,axis=0)
+        gradientAnomaly = (rtgm-np.nanmean(gm,axis=0))/np.nanstd(gm,axis=0)   
+        spreadz = (forecastSprdGrid[t,:,:] - np.nanmean(spread630,axis=0))/np.nanstd(spread630,axis=0)         
         confPlot = (forecastSprdGrid[t,:,:] - mCliMean)/mCliStd                                 
-
         lwMap = np.empty((30,63))
+        lwStd = np.empty((30,63))
         confPlot = np.array(confPlot)
         for x in range (0,30):
             for y in range (0,63):
-                
-                lwMap[x,y] = gradanomaly[x,y]*(gradanomaly[x,y]/(gradanomaly[x,y]+confPlot[x,y])) + confPlot[x,y]*(confPlot[x,y]/(gradanomaly[x,y]+confPlot[x,y]))
-                if lwMap[x,y] > 10 or lwMap[x,y] < -10:
-                    print x, y, lwMap[x,y], gradanomaly[x,y], confPlot[x,y]
-        '''for l in range (0,30):
-            for m in range (0,63):
-                if np.isnan(confPlot[l,m]) == True:
-                    confPlot[l,m] = 10'''
+                lwMap[x,y] = gradMCliMean[x,y]*(gradMCliMean[x,y]/(gradMCliMean[x,y]+mCliMean[x,y])) + mCliMean[x,y]*(mCliMean[x,y]/(gradMCliMean[x,y]+mCliMean[x,y]))
+                lwStd[x,y] = gradMCliStd[x,y]*(gradMCliStd[x,y]/(gradMCliStd[x,y]+mCliStd[x,y])) + mCliStd[x,y]*(mCliStd[x,y]/(gradMCliStd[x,y]+mCliStd[x,y]))
+        LWAMAP = (forecastSprdGrid[t,:,:]-lwMap)/lwStd
                     
-
         tick_nums_SA = np.linspace(-8,8,num=16)
         tick_nums = np.linspace(0,8,num=9)
         g_t = np.linspace(0,10,num=11)
@@ -1086,7 +1088,7 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
         ax5 = plt.subplot2grid((3,2),(2, 0))
         ax6 = plt.subplot2grid((3,2),(2, 1))
         ax1.set_title('Ensemble Mean and Spread')
-        #pdb.set_trace()
+
 
         
         m0 = Basemap(llcrnrlon=265,llcrnrlat=lats.min(),urcrnrlon=lons.max(),urcrnrlat=lats.max(),projection='cyl',resolution='i',ax=ax1)
@@ -1117,14 +1119,14 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
         m1.drawstates()
         
         
-        ax3.set_title('Standardized Spread Anomaly (Gradient M-Climate)')
+        ax3.set_title('Gradient M-Climatology')
         m2 = Basemap(llcrnrlon=265,llcrnrlat=lats.min(),urcrnrlon=lons.max(),urcrnrlat=lats.max(),projection='cyl',resolution='i',ax=ax3)
         ticks = np.linspace(-4,4,num=8)
         x,y = m2(*np.meshgrid(lons,lats))
         mslspace = range(900,1100,2)
         
-        z0 = m2.contourf(x,y,gradanomaly,ticks,cmap=anomcmap,extend='both')
-        cbar = m2.colorbar(z0,ticks=np.linspace(-8,8, num=9),location='bottom',pad='5%')
+        z0 = m2.contourf(x,y,gradMCliMean,cmap=anomcmap,extend='both')
+        cbar = m2.colorbar(z0,location='bottom',pad='5%')
         cbar.set_label(r'$\frac{F_g-\mu}{\sigma}$')
         z1 = m2.contour(x,y,forecastMeanGrid[t,:,:]/100,levels=mslspace,colors='k',linewidths=0.7)
         plt.clabel(z1, fontsize=10, inline_spacing=-1,fmt='%3.0f')
@@ -1132,13 +1134,13 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
         m2.drawcoastlines()
         m2.drawstates()
         
-        ax4.set_title('Standardized Spread Anomaly (Mean M-Climate)')
+        ax4.set_title('Mean M-Climatology')
         m3 = Basemap(llcrnrlon=265,llcrnrlat=lats.min(),urcrnrlon=lons.max(),urcrnrlat=lats.max(),projection='cyl',resolution='i',ax=ax4)
         ticks = np.linspace(-4,4,num=8)
         x,y = m3(*np.meshgrid(lons,lats))
         
-        z0 = m3.contourf(x,y,confPlot,ticks,cmap=anomcmap,extend='both')
-        cbar = m3.colorbar(z0,ticks=np.linspace(-8,8,num=9),location='bottom',pad='5%')
+        z0 = m3.contourf(x,y,mCliMean,cmap=anomcmap,extend='both')
+        cbar = m3.colorbar(z0,location='bottom',pad='5%')
         cbar.set_label(r'$\frac{F_g-\mu_a}{\sigma}$')
         z1 = m3.contour(x,y,forecastMeanGrid[t,:,:]/100,levels=mslspace,colors='k',linewidths=0.7)
         plt.clabel(z1, fontsize=10, inline_spacing=-1,fmt='%3.0f')
@@ -1163,7 +1165,7 @@ def confPm4lw(mslpMean, mslpStd, time, run, day):
         m5 = Basemap(llcrnrlon=265,llcrnrlat=lats.min(),urcrnrlon=lons.max(),urcrnrlat=lats.max(),projection='cyl',resolution='i',ax=ax6)
         x,y = m4(*np.meshgrid(lons,lats))
         
-        z0 = m5.contourf(x,y,lwMap,ticks,cmap=anomcmap,extend='both')
+        z0 = m5.contourf(x,y,LWAMAP,ticks,cmap=anomcmap,extend='both')
         cbar=m5.colorbar(z0,ticks=np.linspace(-8,8,num=9),location='bottom',pad='5%')
         cbar.set_label('hPa')
         z1 = m5.contour(x,y,forecastMeanGrid[t,:,:]/100,levels=mslspace,colors='k',linewidths=0.7)